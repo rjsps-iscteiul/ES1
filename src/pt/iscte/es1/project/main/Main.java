ee4ab0161e75835fe5e65be72cf85f02b5563be5
rjsps-iscteiul <rjsps@iscte-iul.pt>
2017-11-15 09:26:35 +0000

    20171115 - Modify welcome window title

--- src/pt/iscte/es1/project/main/Main.java
+++ src/pt/iscte/es1/project/main/Main.java
@@ -5,7 +5,7 @@ import pt.iscte.es1.project.gui.GUI;
 public class Main {
 
 	public static void main(String[] args) {
-		GUI frame = new GUI("teste");
+		GUI frame = new GUI("Anti-Spam Filter");
 		frame.open();
 	}
 		
